3fb777f832dd57275696cc5eabd79a56f4ca886f
Angela Zhang <angelaz1@andrew.cmu.edu>
2021-11-08 13:10:10 -0500

    Scene changes to m1

--- Assets/Scripts/Minigame1/M1GameManager.cs
+++ Assets/Scripts/Minigame1/M1GameManager.cs
@@ -4,90 +4,20 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class M1GameManager : MonoBehaviour
+public class M1GameManager : GameManager
 {
+    [Header("Minigame 1 Variables")]
     public TextMeshProUGUI totalText;
     public TextMeshProUGUI accuracyText;
-    public TextMeshProUGUI timerText;
-    public GameObject winScreen;
-
-    public int gameTime = 60;
 
     public PrinterController printerController;
-    public GameObject countdownPanel;
-    public TextMeshProUGUI countdownText;
 
     float numCorrect = 0;
     float numTotal = 0;
 
-    [Header("SFX")]
-    AudioSource audioSource;
-    public AudioClip refereeClip;
-    public AudioClip winClip;
-    public AudioClip loseClip;
-
-    private void Start()
+    public override void StartOtherGameObjects()
     {
-        audioSource = GetComponent<AudioSource>();
-        winScreen.SetActive(false);
-        UpdateTimerText();
-        StartCoroutine(StartCountdown());
-    }
-
-    IEnumerator StartCountdown()
-    {
-        // Show a tutorial
-
-        // Start countdown
-        countdownPanel.SetActive(true);
-        countdownText.text = "3";
-        yield return new WaitForSeconds(1f);
-
-        countdownText.text = "2";
-        yield return new WaitForSeconds(1f);
-
-        countdownText.text = "1";
-        yield return new WaitForSeconds(1f);
-
-        countdownText.text = "START!!";
-        audioSource.clip = refereeClip;
-        audioSource.Play();
-        yield return new WaitForSeconds(1f);
-
-        countdownText.text = "";
-        countdownPanel.SetActive(false);
-
         printerController.StartSpawningPapers();
-        StartCoroutine(StartTimer());
-    }
-
-    IEnumerator StartTimer()
-    {
-        while (gameTime > 0)
-        { 
-            UpdateTimerText();
-            yield return new WaitForSeconds(1f);
-            gameTime--;
-        }
-        UpdateTimerText();
-        EndGame();
-    }
-
-    void EndGame()
-    {
-        Time.timeScale = 0;
-        winScreen.SetActive(true);
-
-        // TODO: play clip based on score
-        audioSource.clip = winClip;
-        audioSource.Play();
-    }
-
-    void UpdateTimerText()
-    {
-        string minutes = string.Format("{0:00}", gameTime / 60);
-        string seconds = string.Format("{0:00}", gameTime % 60);
-        timerText.text = $"Time: {minutes}:{seconds}";
     }
 
     public void AddCorrect()
@@ -110,7 +40,7 @@ public class M1GameManager : MonoBehaviour
         accuracyText.text = $"Accuracy: {accuracyStr}%";
     }
 
-    public void ExitToMain()
+    public override void ExitToMain()
     {
         GameObject sManagerObj = GameObject.Find("SelectionManager");
         if (sManagerObj)
