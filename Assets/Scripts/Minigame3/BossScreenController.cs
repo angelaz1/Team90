27da4d01cf4b4b21659d25c7729a1ae7eef551d2
Angela Zhang <angelaz1@andrew.cmu.edu>
2021-11-15 17:05:50 -0500

    Adding in stamp sounds + m3 sounds

--- Assets/Scripts/Minigame3/BossScreenController.cs
+++ Assets/Scripts/Minigame3/BossScreenController.cs
@@ -12,19 +12,36 @@ public class BossScreenController : MonoBehaviour
     public Sprite angryBoss;
     public Sprite ooohBoss;
 
+    [Header("SFX")]
+    public AudioClip happySound;
+    public AudioClip angrySound;
+    public AudioClip sadSound;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void MakeBossAngry()
     {
         bossImage.sprite = angryBoss;
+        audioSource.clip = angrySound;
+        audioSource.Play();
     }
 
     public void MakeBossSad()
     {
         bossImage.sprite = sadBoss;
+        audioSource.clip = sadSound;
+        audioSource.Play();
     }
 
     public void MakeBossOooh()
     {
         bossImage.sprite = ooohBoss;
+        audioSource.clip = happySound;
+        audioSource.Play();
     }
 
     public void MakeBossHappy()
